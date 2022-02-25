f3b366e463883d46901fbdba1db9cda0e98d0a37
Ln028 <nguyenlan50aof@gmail.com>
2022-02-25 11:34:09 +0700

    format code btvn day14

--- techmaster/DAY_14/btvn/main.js
+++ techmaster/DAY_14/btvn/main.js
@@ -3,7 +3,7 @@
 //sortStudents([‘Nam’, ‘Hoa’, ‘Tuấn’]) => [‘Tuấn’, ‘Nam’, ‘Hoa’]
 
 function sortStudents(arr) {
-    return arr.sort((a,b) => b.localeCompare(a))
+    return arr.sort((a, b) => b.localeCompare(a))
 }
 console.log(sortStudents(['Nam', 'Hoa', 'Tuấn']));
 
@@ -15,25 +15,25 @@ console.log(sortStudents(['Nam', 'Hoa', 'Tuấn']));
 //thuật toán Fisher–Yates shuffle
 
 function array(arr) {
-    for (let i = arr.length-1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i+1))
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
         let ele = arr[i]
         arr[i] = arr[j]
         arr[j] = ele
     }
     return arr
-    
+
 }
-console.log(array([1,2,3,4,5]));
+console.log(array([1, 2, 3, 4, 5]));
 
 //Bài 3: Viết function để lấy sự phần tử không xuất hiện ở cả 2 mảng
 
 //symmetricDifference([1, 2, 3], [1, 2, 4]) => [3,4]
 
 function arrayDif(arr1, arr2) {
-   let newArr1 = arr1.filter(value => !arr2.includes(value))
-   let newArr2 = arr2.filter(value => !arr1.includes(value))
-   return newArr1.concat(newArr2)
+    let newArr1 = arr1.filter(value => !arr2.includes(value))
+    let newArr2 = arr2.filter(value => !arr1.includes(value))
+    return newArr1.concat(newArr2)
 }
 console.log(arrayDif([1, 2, 3], [1, 2, 4, 5, 6]));
 
@@ -43,9 +43,9 @@ console.log(arrayDif([1, 2, 3], [1, 2, 4, 5, 6]));
 //union([1, 2, 3, 2, 3], [1, 2, 3, 1, 2]) => [1,2,3]
 
 function unicon(arr1, arr2) {
-    let unionArr = arr1.filter(value => arr2.includes(value))
-    let unionArr1 = arr1.filter(value => !arr2.includes(value))
-    let unionArr2 = arr2.filter(value => !arr1.includes(value))
-    return unionArr.concat(unionArr1).concat(unionArr2)
- }
- console.log(unicon([1, 2, 3], [1, 2, 3, 5, 6]));
+    let union1 = arr1.filter(value => arr2.includes(value))
+    let union2 = arr1.filter(value => !arr2.includes(value))
+    let union3 = arr2.filter(value => !arr1.includes(value))
+    return union1.concat(union2).concat(union3)
+}
+console.log(unicon([1, 2, 3], [1, 2, 3, 5, 6]));
