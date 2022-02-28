764c2d3ff4a60aca7e76977836a8db374a4de66d
Ln028 <nguyenlan50aof@gmail.com>
2022-02-28 13:13:54 +0700

    btvn html week2

--- techmaster/DAY_15/btvn/btvn grade/main.js
+++ techmaster/DAY_15/btvn/btvn grade/main.js
@@ -11,7 +11,7 @@ const grades = [
     { name: 'Jane', grade: 9, sex: 'F' }
 ]
 
-//Viết function tính thứ hạng trung bình của cả lớp
+//1.Viết function tính thứ hạng trung bình của cả lớp
 const showRating = rating => {
     let rate = 0
     rating.forEach(student => {
@@ -21,7 +21,7 @@ const showRating = rating => {
 }
 console.log(showRating(grades));
 
-//Viết function tính thứ hạng trung bình của nam trong lớp
+//2.Viết function tính thứ hạng trung bình của nam trong lớp
 // const showMale = male => {
 //     return male.filter(student => student.sex == "M") 
 // }
@@ -48,7 +48,7 @@ const showMale = male => {
 }
 console.log(showMale(grades));
 
-//Viết function tính thứ hạng trung bình của Nữ trong lớp
+//3.Viết function tính thứ hạng trung bình của Nữ trong lớp
 
 const showFemale = female => {
     let rateFemale = 0;
@@ -64,74 +64,80 @@ const showFemale = female => {
 }
 console.log(showFemale(grades));
 
-//Viết function tìm học viên Nam có thứ hạng cao nhất trong lớp
+//4.Viết function tìm học viên Nam có thứ hạng cao nhất trong lớp
 
 const firstMale = first1 => {
     let maleTop = first1.filter(student => student.sex == "M").sort((a, b) => b.grade - a.grade)
-    return maleTop[0]
+     let maleTop1 = maleTop.filter(student => student.grade == maleTop[0].grade)
+     return maleTop1
 }
 console.log(firstMale(grades));
 
-//Viết function tìm học viên Nữ có thứ hạng cao nhất trong lớp
+//5.Viết function tìm học viên Nữ có thứ hạng cao nhất trong lớp
 const firstFemale = first2 => {
     let femaleTop = first2.filter(student => student.sex == "F").sort((a, b) => b.grade - a.grade)
-    return femaleTop[0]
+    let femaleTop1 = femaleTop.filter(student => student.grade == femaleTop[0].grade)
+    return femaleTop1
 }
 console.log(firstFemale(grades));
 
-//Viết function tìm học viên Nam có thứ hạng thấp nhất trong lớp
+// 6.Viết function tìm học viên Nam có thứ hạng thấp nhất trong lớp
 const lastMale = last1 => {
     let showMale = last1.filter(student => student.sex == "M").sort((a, b) => a.grade - b.grade)
-    return showMale[0]
+    let showMale1 = showMale.filter(student => student.grade == showMale[0].grade)
+    return showMale1
 }
 console.log(lastMale(grades));
 
-//Viết function tìm học viên Nữ có thứ hạng thấp nhất trong lớp
+// 7.Viết function tìm học viên Nữ có thứ hạng thấp nhất trong lớp
 const lastFemale = last2 => {
     let showFemale = last2.filter(student => student.sex == "F").sort((a, b) => a.grade - b.grade)
-    return showFemale[0]
+    let showFemale1 = showFemale.filter(student => student.grade == showFemale[0].grade)
+    return showFemale1
 }
 console.log(lastFemale(grades));
 
-//Viết function thứ hạng cao nhất của cả lớp
+// 8.Viết function thứ hạng cao nhất của cả lớp
 const topNo1 = student1 => {
     let top1 = student1.sort((a, b) => b.grade - a.grade)
-    return top1[0]
+    let showTop1 = top1.filter(student => student.grade == top1[0].grade)
+    return showTop1
 }
 console.log(topNo1(grades));
 
-//Viết function thứ hạng thấp nhất của cả lớp
+// 9.Viết function thứ hạng thấp nhất của cả lớp
 const lastNo1 = student2 => {
     let last1 = student2.sort((a, b) => a.grade - b.grade)
-    return last1[0]
+    let showLast1 = last1.filter(student => student.grade == last1[0].grade)
+    return showLast1
 }
 console.log(lastNo1(grades));
 
-//Viết function lấy ra danh sách tất cả học viên Nữ trong lớp
+//10.Viết function lấy ra danh sách tất cả học viên Nữ trong lớp
 const nameFemale = listFemale => {
     return listFemale.filter(student => student.sex == "F")
 }
 console.log(nameFemale(grades));
 
-//Viết function sắp xếp tên học viên theo chiều tăng dần của bảng chữ cái
+//11.Viết function sắp xếp tên học viên theo chiều tăng dần của bảng chữ cái
 const arrangeName = arrange => {
     return arrange.sort((a, b) => (a.name).localeCompare((b.name)))
 }
 console.log(arrangeName(grades));
 
-//Viết function sắp xếp thứ hạng học viên theo chiều giảm dần
+//12.Viết function sắp xếp thứ hạng học viên theo chiều giảm dần
 const arrangeGrade = arrange => {
     return arrange.sort((a, b) => a.grade - b.grade)
 }
 console.log(arrangeGrade(grades));
 
-//Viết function lấy ra học viên Nữ có tên bắt đầu bằng “J”
+//13.Viết function lấy ra học viên Nữ có tên bắt đầu bằng “J”
 const startWithName = startWith => {
     return startWith.filter(student => student.sex == "F" && (student.name).startsWith("J"))
 }
 console.log(startWithName(grades));
 
-//Viết function lấy ra top 5 người có thứ hạng cao nhất trong lớp
+//14.Viết function lấy ra top 5 người có thứ hạng cao nhất trong lớp
 const topFive = allStudent => {
     let top5 = allStudent.sort((a, b) => b.grade - a.grade)
     return top5.slice(0, 5)
