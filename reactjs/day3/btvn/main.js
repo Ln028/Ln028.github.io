8897d3393458b4b14dd29c5dd89fa168971a7e57
Ln028 <nguyenlan50aof@gmail.com>
2022-06-10 15:05:30 +0700

    updated btvn day 3 react

--- reactjs/day3/btvn/main.js
+++ reactjs/day3/btvn/main.js
@@ -7,25 +7,21 @@
 var multiply = function(num1, num2) {
     var len1 = num1.length;
     var len2 = num2.length;
-    var res = Array(len1 + len2).fill(0);
-    var carry = 0;
-    var val = 0;
-    var index = 0;
-  
-    for (var i = len1 - 1; i >= 0; i--) {
-      carry = 0;
-      for (var j = len2 - 1; j >= 0; j--) {
-        index = len1 + len2 - 2 - i - j;
-        val= (num1[i] * num2[j]) + carry + res[index];
-        carry = Math.floor(val / 10);
-        res[index] = val % 10;
+    var result = Array(len1 + len2).fill(0);
+    for (var j = len2 - 1; j >= 0; j--) {
+      for (var i = len1 - 1; i >= 0; i--) {
+        const product = num1[i] * num2[j]
+        const index = len1 + len2 - 1 - [len2 - 1 - j + len1 - 1 - i];
+        result[index] += product
+        if(result[index] > 9) {
+            result[index - 1] += Math.floor(result[index] / 10)
+            result[index] = result[index] % 10
+        }
       }
-      if (carry) res[index + 1] = carry;
     }
+    while (result[0] === 0) result.shift();
   
-    while (res.length > 1 && res[res.length - 1] === 0) res.pop();
-  
-    return res.reverse().join('');
+    return result.length === 0 ? 0 : result.join('');
   };
 
 
